86985781b87d7849b1d0969138df74682422b09a
Ilya <ilya@code-care.pro>
2022-03-19 13:49:51 +0200

    feature(user): add user entity to imports list

--- sociability-backend/src/app.module.ts
+++ sociability-backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
 
 @Module({
   imports: [
@@ -12,12 +14,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'admin',
       password: 'admin',
       database: 'postgres',
-      // entities: [join(__dirname, '**', '*.entity.{ts, js}')],
-      entities: [],
+      entities: [User],
+      // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       synchronize: true,
       logging: true,
       logger: 'advanced-console',
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
